735de8796990a70aa4888f5afcdd200b88f92326
helladyo <heladiofog@gmail.com>
2019-03-22 00:44:07 -0600

    Finishing and uploading playground of the course.

---
+++ intro_node/app/models/vehicle.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var VehicleSchema = new Schema({
+    make: String,
+    model: String,
+    color: String
+});
+
+module.exports = mongoose.model('Vehicle', VehicleSchema);
+
+/*
+ES6
+import { Schema as _Schema, model as _model } from 'mongoose';
+var Schema = _Schema;
+
+var VehicleSchema = new Schema({
+    make: String,
+    model: String,
+    color: String
+});
+
+export default _model('Vehicle', VehicleSchema);
+*/
