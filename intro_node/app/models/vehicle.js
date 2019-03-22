8954c2642468575c028c4229a1c66f592e1dbda8
helladyo <heladiofog@gmail.com>
2019-03-22 02:18:30 -0600

    Details of SP-Intro Node API course.

--- intro_node/app/models/vehicle.js
+++ intro_node/app/models/vehicle.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var VehicleSchema = new Schema({
-    make: String,
-    model: String,
-    color: String
+  make: String,
+  model: String,
+  color: String
 });
 
 module.exports = mongoose.model('Vehicle', VehicleSchema);
--- intro_node/server.js
+++ intro_node/server.js
@@ -17,10 +17,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Set up port for server to listen on
-let port = process.env.PORT || 3000;
+let port = process.env.NODE_PORT || 3000;    // NODE_PORT=5000 on local SO env
 
 // Connect to our database
-mongoose.connect('mongodb://localhost:27017/spCodealong');
+mongoose.connect('mongodb://localhost:27017/spCodealong',
+  {
+    useNewUrlParser: true
+  }
+);
 
 // API routes
 let router = express.Router();
@@ -33,77 +37,86 @@ app.use('/api/v1', router);
 // thinngs from here or stop the request from continuing in
 // the event that the request is not safe.
 // Middleware to use for all requests
-router.use(function(req, res, next) {
-    console.log('FYI...There is some processing currently going down...');
-    // The very last thing it does is to call next...
-    next();
+router.use(function (req, res, next) {
+  console.log('FYI...There is some processing currently going down...');
+  // The very last thing it does is to call next...
+  next();
 });
 
-// Test Route
+// Test Route, since router has been set to /api/v1, the whole URI is:
+// http://localhost:5000/api/v1
 router.get('/', (req, res) => {
-    res.status(200)
-        .json({ message: 'Welcome to playground API.' });
+  console.log("Root path on http://localhost:5000/api/v1");
+  res.status(200)
+    .json({ message: 'Welcome to playground API.' });
 })
 
+// Routes enchained
 router.route('/vehicles')
-    .post(function (req, res) {
-        var vehicle = new VehicleModel();   // new instance of a vehicle
-        vehicle.make = req.body.make;
-        vehicle.model = req.body.model;
-        vehicle.color = req.body.color;
+  // Creates a new vehicle
+  // Test URL: http://localhost:5000/api/v1/vehicles/
+  // The data goes through POST method
+  .post(function (req, res) {
+    var vehicle = new VehicleModel();   // new instance of a vehicle
+    vehicle.make = req.body.make;
+    vehicle.model = req.body.model;
+    vehicle.color = req.body.color;
 
-        vehicle.save(function (err, data) {
-            if (err) {
-                res.status(500).send(err);
-            }
+    vehicle.save(function (err, data) {
+      if (err) {
+        res.status(500).send(err);
+      }
 
-            res.status(200)
-                .json({ message: 'Vehicle susccessfully manufactured'});
-        });
-    })
-
-    .get(function (req, res) {
-        VehicleModel.find(function (err, vehicles){
-            if (err) {
-                res.status(400).send(err);
-            }
-
-            res.status(200).json(vehicles);
-        });
+      res.status(200)
+        .json({ message: 'Vehicle susccessfully manufactured' });
     });
+  })
+  // Return all the vehicles
+  // Test URL: http://localhost:5000/api/v1/vehicles/5b137960c202171e327490a8
+  .get(function (req, res) {
+    VehicleModel.find(function (err, vehicles) {
+      if (err) {
+        res.status(400).send(err);
+      }
 
+      res.status(200).json(vehicles);
+    });
+  });
+
+// GEt vehicles by some filter
+// Parameter notation with ":", use req.params
 router.route('/vehicles/:vehicle_id')
-    .get(function (req, res) {
-        VehicleModel.findById(req.params.vehicle_id, function (err, vehicle) {
-            if (err) {
-                res.status(400).send(err);
-            }
+  .get(function (req, res) {
+    VehicleModel.findById(req.params.vehicle_id, function (err, vehicle) {
+      if (err) {
+        res.status(400).send(err);
+      }
 
-            res.status(200).json(vehicle);
-        });
+      res.status(200).json(vehicle);
     });
+  });
 
 router.route('/vehicles/make/:make')
-    .get(function (req, res) {
-        VehicleModel.find({ make: req.params.make }, function (err, vehicle) {
-            if (err) {
-                res.status(400).send(err);
-            }
+  .get(function (req, res) {
+    VehicleModel.find({ make: req.params.make }, function (err, vehicle) {
+      if (err) {
+        res.status(400).send(err);
+      }
 
-            res.status(200).json(vehicle);
-        });
+      res.status(200).json(vehicle);
     });
+  });
 
 router.route('/vehicles/color/:color')
-    .get(function (req, res) {
-        VehicleModel.find({ color: req.params.color }, function (err, vehicle) {
-            if (err) {
-                res.status(400).send(err);
-            }
+  .get(function (req, res) {
+    VehicleModel.find({ color: req.params.color }, function (err, vehicle) {
+      if (err) {
+        res.status(400).send(err);
+      }
 
-            res.status(200).json(vehicle);
-        });
+      res.status(200).json(vehicle);
     });
+  });
 
 // Fire up server
 app.listen(port);
