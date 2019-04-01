d479014871a6ea7e3938840358680844921fe04a
Heladio Atreaga Franco <harteagaf@atencion.com>
2019-04-01 12:51:53 -0600

    Updating some packages and code.

--- intro_react/swap-shop-web/src/services/http-service.js
+++ intro_react/swap-shop-web/src/services/http-service.js
@@ -3,24 +3,24 @@ import 'whatwg-fetch';
 
 // A new class with ES6
 class HttpService {
-    getProducts = () => {
-        // 1
-        var promise = new Promise((resolve, reject) => {
-            // 2
-            fetch('http://localhost:3004/product')
-            .then(response => {
-                // 4 At last
-                // console.log(response.json());
-                resolve(response.json());
-                // reject("You suck!");    // Testing the promise sent 
-            })
-            .catch(error => {
-                console.log("Error on promise" + error);
-            });
+  getProducts = () => {
+    // 1
+    let promise = new Promise((resolve, reject) => {
+      // 2
+      fetch('http://localhost:3004/product')
+        .then(response => {
+          // 4 At last
+          // console.log(response.json());
+          resolve(response.json());
+          // reject("You suck!");    // Testing the promise sent 
+        })
+        .catch(error => {
+          console.log("Error on promise " + error);
         });
-        // 3. Return the promise immediately
-        return promise;
-    }
+    });
+    // 3. Return the promise immediately
+    return promise;
+  }
 }
 
 export default HttpService;
